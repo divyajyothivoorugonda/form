104562acc5eba712bf86a7d76390583c89b8f039
divyajyothivoorugonda <55979031+divyajyothivoorugonda@users.noreply.github.com>
2023-06-04 16:46:45 +1000

    Added condition before adding the task card

--- form.js
+++ form.js
@@ -4,9 +4,14 @@ function myFunction() {
     let assignedTo = document.getElementById("assignedTo").value;
     let dueDate = document.getElementById("dueDate").value;
     let status = document.getElementById("status").value;
-    var taskData = [];
-    taskData[taskData.length] = [name,desc,assignedTo,dueDate,status];
-    console.log(taskData.length);
-    document.getElementById('card-deck').innerHTML += 
-    '<br/><div class="card" id="cardID'+ taskData.length +'"><div class="card-body bg-light"><h5 class="card-title">'+name+'</h5><h6 class="card-subtitle mb-2 text-muted">Assigned to: '+ assignedTo +'</h6><p class="card-text">' + desc + '</p><p class="card-text">Due date: '+ dueDate+'</p><p class="card-text">Status: '+status+' </p><button type="submit" class="btn btn-primary mt-2" onclick="">Delete</button></div>';
+    if(name != '' && desc!='' && assignedTo!=''){
+        var taskData = [];
+        taskData[taskData.length] = [name,desc,assignedTo,dueDate,status];
+        console.log(taskData.length);
+        document.getElementById('card-deck').innerHTML += 
+        '<br/><div class="card" id="cardID'+ taskData.length +'"><div class="card-body bg-light"><h5 class="card-title">'+name+'</h5><h6 class="card-subtitle mb-2 text-muted">Assigned to: '+ assignedTo +'</h6><p class="card-text">' + desc + '</p><p class="card-text">Due date: '+ dueDate+'</p><p class="card-text">Status: '+status+' </p><button type="submit" class="btn btn-primary mt-2" onclick="">Delete</button></div>';
+    }
+    else{
+        alert('Please fill all required fields');
+    }
 }
